458039216706a343542a5f517ee890c9b97c37be
sethvincent <sethvincent@gmail.com>
2016-12-10 11:04:22 -0800

    add has/delete functions, fix split bug, make set/delete return new objects

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var isString = require('is-string')
 var isNumber = require('is-number')
 var isArray = require('isarray')
+var xtend = require('xtend')
 
 module.exports = {
   get: get,
-  set: set
+  set: set,
+  has: has,
+  delete: del
 }
 
 function get (data, key, split) {
-  if (!key) return data
+  if (!key) return xtend(data)
   var keys = parseKeys(key, split)
   var current = keys[0]
 
@@ -23,20 +26,45 @@ function get (data, key, split) {
   return get(data[current], keys.slice(1))
 }
 
-function set (data, key, value, split) {
+function set (data, key, value, split, original) {
+  original = original || data
+  var keys = parseKeys(key, split)
+  var current = keys[0]
+  var next = keys[1]
+
+  if (keys && keys.length && keys.length === 1) {
+    data[current] = value
+    var out = original || data
+    return xtend(out)
+  }
+
+  if (next && data[current] === undefined) {
+    if (isString(next)) {
+      data[current] = {}
+    } else if (isNumber(next)) {
+      data[current] = []
+    }
+  }
+
+  return set(data[current], keys.slice(1), value, split, original)
+}
+
+function has (data, key, split) {
+  return !!get(data, key, split)
+}
+
+function del (data, key, value, split, original) {
+  original = original || data
   var keys = parseKeys(key, split)
   var current = keys[0]
 
   if (keys && keys.length && keys.length === 1) {
-    data[current] = value
-    return data
+    delete data[current]
+    var out = original || data
+    return xtend(out)
   }
 
-  if (data[current] === undefined) {
-    data[current] = {}
-  }
-
-  return set(data[current], keys.slice(1), value)
+  return del(data[current], keys.slice(1), value, split, original)
 }
 
 function parseKeys (key, split) {
--- tests/index.js
+++ tests/index.js
@@ -12,9 +12,11 @@ test('get and set value', function (t) {
 
   var value = keypath.get(data, 'nested.obj.arr.0.cool')
   t.equal(value, 'pizza')
-  keypath.set(data, 'nested.obj.arr.0.cool', 'awesome')
+  var updated = keypath.set(data, 'nested.obj.arr.0.cool', 'awesome')
   value = keypath.get(data, 'nested.obj.arr.0.cool')
   t.equal(value, 'awesome')
+  t.equal(keypath.get(updated, 'nested.obj.arr.0.cool'), 'awesome')
+  t.notEqual(data, updated, 'set returns new object')
   t.end()
 })
 
@@ -43,6 +45,25 @@ test('getting missing keys returns undefined', function (t) {
 
   value = keypath.get(data, 'missing.nested')
   t.equal(value, undefined)
+  t.end()
+})
+
+test('has a value', function (t) {
+  var data = { example: 'ok' }
+
+  var exists = keypath.has(data, 'example')
+  t.equal(exists, true)
+
+  var missing = keypath.has(data, 'missing')
+  t.equal(missing, false)
+  t.end()
+})
+
+test('delete values', function (t) {
+  var data = { example: 'ok' }
+
+  var updated = keypath.delete(data, 'example')
+  t.notOk(updated.example)
   t.end()
 })
 
